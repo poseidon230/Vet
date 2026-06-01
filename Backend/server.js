8ed86a45ece347636d59963e6a6af7e748d7781f
Sebastian24 <165705681+poseidon230@users.noreply.github.com>
2026-06-01 10:40:17 -0600

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,14 +5,14 @@ const Producto = require('./models/Producto');
 const Usuario = require('./models/Usuario');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 conectarDB();
 
 app.use(cors());
 app.use(express.json());
 
-// 1. OBTENER PRODUCTOS
+// OBTENER PRODUCTOS
 app.get('/api/productos', async (req, res) => {
     try {
         const productos = await Producto.find();
@@ -22,7 +22,7 @@ app.get('/api/productos', async (req, res) => {
     }
 });
 
-// 2. CREAR PRODUCTO
+// CREAR PRODUCTO
 app.post('/api/productos', async (req, res) => {
     try {
         const nuevoProd = new Producto(req.body);
@@ -33,36 +33,53 @@ app.post('/api/productos', async (req, res) => {
     }
 });
 
-// 3. REGISTRAR USUARIO
+// REGISTRAR USUARIO
 app.post('/api/usuarios', async (req, res) => {
     try {
         const { correo } = req.body;
+
         const usuarioExistente = await Usuario.findOne({ correo });
+
         if (usuarioExistente) {
-            return res.status(400).json({ error: 'El correo electrónico ya está registrado.' });
+            return res.status(400).json({
+                error: 'El correo electrónico ya está registrado.'
+            });
         }
+
         const nuevoUsuario = new Usuario(req.body);
         await nuevoUsuario.save();
+
         res.status(201).json(nuevoUsuario);
     } catch (error) {
         res.status(400).json({ error: 'Error al registrar usuario.' });
     }
 });
 
-// 4. LOGIN DE USUARIO
+// LOGIN
 app.post('/api/usuarios/login', async (req, res) => {
     try {
         const { correo, contrasena } = req.body;
-        const usuario = await Usuario.findOne({ correo, contrasena });
+
+        const usuario = await Usuario.findOne({
+            correo,
+            contrasena
+        });
+
         if (!usuario) {
-            return res.status(401).json({ error: 'Credenciales inválidas.' });
+            return res.status(401).json({
+                error: 'Credenciales inválidas.'
+            });
         }
-        res.status(200).json({ nombre: usuario.nombre, correo: usuario.correo });
+
+        res.status(200).json({
+            nombre: usuario.nombre,
+            correo: usuario.correo
+        });
     } catch (error) {
         res.status(500).json({ error: 'Error en el servidor.' });
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`📡 Servidor de VetCare corriendo en: http://localhost:${PORT}`);
+    console.log(`📡 VetCare ejecutándose en el puerto ${PORT}`);
 });
